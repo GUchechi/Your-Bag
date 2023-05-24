08f41765a8e549529bcf9d5e3d190aa321c9ac0a
GUchechi <godswilluchendu7@gmail.com>
2023-05-24 12:42:42 +0100

    cartContainer

--- src/components/CartContainer.jsx
+++ src/components/CartContainer.jsx
@@ -1,12 +1,20 @@
-
-import { useSelector } from 'react-redux';
+import { useSelector } from "react-redux";
 
 const CartContainer = () => {
-    const { cartItems, total, amount } = useSelector((store) => store.cart);
+  const { cartItems, total, amount } = useSelector((store) => store.cart);
 
-  return (
-    <div>CartContainer</div>
-  )
-}
+  if (amount < 1) {
+    return (
+      <section className="cart">
+        <header>
+          <h2>your bag</h2>
+          <h4 className="empty-cart">is currently empty</h4>
+        </header>
+      </section>
+    );
+  }
 
-export default CartContainer
+  return <div>CartContainer</div>;
+};
+
+export default CartContainer;
