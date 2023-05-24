9a3e5096873d26bcff9961a0cac73c8f4d29c1cb
GUchechi <godswilluchendu7@gmail.com>
2023-05-24 16:14:38 +0100

    cart

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -50,7 +50,11 @@ const cartSlice = createSlice({
     [getCartItems.pending]: (state) => {
       state.isLoading = true;
     },
-    
+    [getCartItems.fulfilled]: (state, action) => {
+      console.log(action);
+      state.isLoading = false;
+      state.cartItems = action.payload;
+    },
   },
 });
 
